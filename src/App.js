55fb59ea307c1c63196b30bf73900c6e66884dc9
Feline Huhn <feline.huhn@gmx.de>
2024-02-02 12:10:17 +0100

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("Button clicked");
+  }
+
+  return <Button color="hotpink" text="Click me!" onClick={handleClick} />;
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button style={{ color: color }} disabled={disabled} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
